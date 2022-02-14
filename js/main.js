1468a488a56cc5e86fe496be7848116760078f6b
Lith Turkin <lithiumturkin@protonmail.com>
2022-02-14 19:29:27 +0100

    Update Case to an Array

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-// let form = new FormData();
-
-// console.log(Data.append("text", document.getElementById("uname").value));
 const button = document.querySelector('#ask-question');
 let userName = 'Joe Biden';
 let userQuestion = 'Will I die';
@@ -15,59 +12,31 @@ function handleButtonClick(event)
   userQuestion = document.querySelector('#question').value;
 
 
-
-  let randomNumber = Math.floor(Math.random() * 8);
   let eightBall = '';
   let p_tag = document.querySelector('#result');
 
-  switch (randomNumber) {
-    case 1:
-      eightBall = 'It is certain';
-      break;
-    case 2:
-      eightBall = 'It is decidedly so';
-      break;
-    case 3:
-      eightBall = 'Reply hazy try again';
-      break;
-    case 4:
-      eightBall = 'Cannot predict now';
-      break;
-    case 5:
-      eightBall = 'Do not count on it';
-      break;
-    case 6:
-      eightBall = 'My sources say no';
-      break;
-    case 7:
-      eightBall = 'Outlook not so good';
-      break;
-    case 8:
-      eightBall = 'Signs point to yes';
-      break;
-  }
+const eightBallAnswers = new Array(
+    'It is certain',
+    'It is decidedly so',
+    'Reply hazy try again',
+    'Cannot predict now',
+    'Do not count on it',
+    'My sources say no',
+    'Outlook not so good',
+    'Signs point to yes'
+);
 
-  if (userQuestion === '') {
-    p_tag.innerHTML = 'Please Fill in the Question'
-  } else {
-    p_tag.innerHTML = userName === '' ? 'Hello!' : `Hello ${userName}!`;
-    p_tag.innerHTML += '<br />' + 'Question: ' + (userName === '' ? userQuestion : `${userName} asked ${userQuestion}`);
-    p_tag.innerHTML += '<br /><br />' + eightBall;
-  }
+let randomNumber = Math.floor(Math.random() * eightBallAnswers.length);
+
+eightBall = eightBallAnswers[randomNumber]
+
+   if (userQuestion === '') {
+     p_tag.innerHTML = 'Please Fill in the Question'
+   } else {
+     p_tag.innerHTML = userName === '' ? 'Hello!' : `Hello ${userName}!`;
+     p_tag.innerHTML += '<br />' + 'Question: ' + (userName === '' ? userQuestion : `${userName} asked ${userQuestion}`);
+     p_tag.innerHTML += '<br /><br />' + eightBall;
+   }
   
 
 }
-
-
-// userName ? console.log(`Hello ${userName}!`)
-// : console.log('Hello!');
-
-// if (userName == '') {
-//   console.log(userQuestion);
-// } else {
-//   console.log(`${userName} Asked ` + userQuestion);
-// }
-
-
-
-// console.log(eightBall);
